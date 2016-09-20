3f086083966a7c83642fd0c62adfe39f4aee47d3
NTD <kaanoo2904@gmail.com>
2016-09-20 20:49:03 +0700

    fix assetable relationship

--- lib/ckeditor/orm/active_record.rb
+++ lib/ckeditor/orm/active_record.rb
@@ -1,24 +1,24 @@
 require 'ckeditor/orm/base'
 
 module Ckeditor
-  module Orm
-    module ActiveRecord
-      module AssetBase
-        def self.included(base)
-          base.send(:include, Base::AssetBase::InstanceMethods)
-          base.send(:extend, ClassMethods)
-        end
+    module Orm
+        module ActiveRecord
+            module AssetBase
+                def self.included(base)
+                    base.send(:include, Base::AssetBase::InstanceMethods)
+                    base.send(:extend, ClassMethods)
+                end
 
-        module ClassMethods
-          def self.extended(base)
-            base.class_eval do
-              self.table_name = 'ckeditor_assets'
+                module ClassMethods
+                    def self.extended(base)
+                        base.class_eval do
+                            self.table_name = 'ckeditor_assets'
 
-              belongs_to :assetable, polymorphic: true
+                            belongs_to :assetable, polymorphic: true, optional: true
+                        end
+                    end
+                end
             end
-          end
         end
-      end
     end
-  end
 end
